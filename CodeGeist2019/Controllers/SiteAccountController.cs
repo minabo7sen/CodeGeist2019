423dbb7ab94fd06114b748b316497ac29c2d3faf
Mina Mohsen <minamhakim@gmail.com>
2019-07-29 15:17:45 +0200

    Added new account Creation

---
+++ CodeGeist2019/Controllers/SiteAccountController.cs
@@ -0,0 +1,65 @@
+﻿using CodeGeist2019.Models;
+using CodeGeist2019.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeGeist2019.Controllers
+{
+    public class SiteAccountController : Controller
+    {
+        private ApplicationDbContext _context;
+        public SiteAccountController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: SiteAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            UserViewModel uv = new UserViewModel();
+            return View("New", uv);
+        }
+
+        [HttpPost]
+        public ActionResult Save(UserViewModel uvm)
+        {
+            var id = User.Identity.GetUserId();
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+
+            if (uvm.Designer == true)
+            {
+                UserManager.AddToRole(id, "Designer");
+            }
+
+            if (uvm.Translator == true)
+            {
+                UserManager.AddToRole(id, "Translator");
+            }
+
+
+            Account account = new Account
+            {
+                Name = uvm.NewUser.Name,
+                Age = uvm.NewUser.Age,
+                Gender = uvm.NewUser.Gender,
+                //etc...
+
+            };
+
+
+            _context.Account.Add(account);
+            _context.SaveChanges();
+            return View("../Home/Index");
+        }
+    }
+}
---
+++ CodeGeist2019/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CodeGeist2019.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeGeist2019.ViewModels
+{
+    public class UserViewModel
+    {
+        public Account NewUser { get; set; }
+        public bool Designer { get; set; }
+        public bool Translator { get; set; }
+    }
+}
