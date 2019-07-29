b81dc383bb1a3ba966ce4fdce730ce5e322cb9d4
Mina Mohsen <minamhakim@gmail.com>
2019-07-29 11:59:49 +0200

    Initial Migration

---
+++ CodeGeist2019/Migrations/Configuration.cs
@@ -0,0 +1,38 @@
+namespace CodeGeist2019.Migrations
+{
+    using CodeGeist2019.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeGeist2019.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodeGeist2019.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+
+            //var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            //var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+
+            //string roleName = "Admin";
+            //IdentityResult roleResult;
+
+            //if (!RoleManager.RoleExists(roleName))
+            //{
+            //    roleResult = RoleManager.Create(new IdentityRole(roleName));
+            //}
+        }
+    }
+}
