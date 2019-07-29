cf84c3d80257e0b4a7b48eff1cf3b76fc3aa485a
Mina Mohsen <minamhakim@gmail.com>
2019-07-29 10:42:05 +0200

    Add project files.

---
+++ CodeGeist2019/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeGeist2019.MvcApplication" Language="C#" %>
---
+++ CodeGeist2019/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeGeist2019.Startup))]
+namespace CodeGeist2019
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
